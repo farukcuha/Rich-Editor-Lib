2904f2cba25ab74bb453099c624e2f6f3d220379
ahmetfarukcuha <ahmetfarukcuha@hotmail.com>
2022-06-19 02:04:58 +0300

    quickfixes

--- app/src/main/java/com/easyapps/richeditor/MainActivity.kt
+++ app/src/main/java/com/easyapps/richeditor/MainActivity.kt
@@ -29,11 +29,8 @@ class MainActivity : AppCompatActivity() {
                 "    <p><b><span style=\"font-size:18px\";><span style=\"color:#000000;\">Bold</span></span></b></p>\n" +
                 "    <p><i><span style=\"font-size:18px\";><span style=\"color:#000000;\">Italic</span></span></i></p>\n" +
                 "    <p><u><span style=\"font-size:18px\";><span style=\"color:#000000;\">Undeline</span></span></u></p>\n" +
-                "    <p><span style=\"text-decoration:line-through;\"><span style=\"font-size:18px\";><span style=\"color:#000000;\">Strike through</span></span></span></p>\n" +
-                "    <p><span style=\"font-size:26px\";><span style=\"color:#000000;\">Text Size</span></span></p>\n" +
-                "    <p><span style=\"color:#000000;\"><span style=\"font-size:18px\";>Colored text like </span></span><span style=\"color:#FF0000;\"><span style=\"font-size:18px\";>red </span></span><span style=\"color:#FFEB3B;\"><span style=\"font-size:18px\";>yellow </span></span><span style=\"font-size:18px\";><span style=\"color:#03A9F4;\">...</span></span></p>\n" +
+                "    <p><span style=\"color:#000000;\"><span>Colored text like </span></span><span style=\"color:#FF0000;\"><span style=\"font-size:18px\";>red </span></span><span style=\"color:#FFEB3B;\"><span style=\"font-size:18px\";>yellow </span></span><span style=\"font-size:18px\";><span style=\"color:#03A9F4;\">...</span></span></p>\n" +
                 "    <br>\n" +
-                "    <p><b><i><u><span style=\"text-decoration:line-through;\"><span style=\"font-size:32px\";><span style=\"color:#03A9F4;\">All </span></span></span></u></i></b><b><i><u><span style=\"text-decoration:line-through;\"><span style=\"font-size:32px\";><span style=\"color:#FF0000;\">in </span></span></span></u></i></b><b><i><u><span style=\"text-decoration:line-through;\"><span style=\"font-size:32px\";><span style=\"color:#607D8B;\">one </span></span></span></u></i></b><b><i><u><span style=\"text-decoration:line-through;\"><span style=\"font-size:32px\";><span style=\"color:#CDDC39;\">text!</span></span></span></u></i></b></p>\n" +
                 "    </body>\n" +
                 "    </html>"
         richEditText.setHtml(html)
